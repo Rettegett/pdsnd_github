452efc4632e27b271dfd243c3925936008b6c908
Tamas Danos <danostomi@hotmail.com>
2020-10-05 17:26:26 +0200

    Bikeshare file: removed unnecessary line comments

--- bikesharev2.py
+++ bikesharev2.py
@@ -7,14 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
 
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     cities={ 'chicago': 'chicago',
@@ -78,16 +71,7 @@ def get_filters():
     return city, month, day
 
 def load_data(city, month, day):
-    """
-    Loads data for the specified city and filters by month and day if applicable.
 
-    Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
-    """
     df=pd.read_csv('{}.csv'.format(city))
 
     df['Start Time'] = pd.to_datetime(df['Start Time'])
@@ -116,7 +100,7 @@ def load_data(city, month, day):
 
 def time_stats(df):
 
-    """Displays statistics on the most frequent times of travel."""
+
     df['hour'] = df['Start Time'].dt.hour
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
@@ -138,7 +122,7 @@ def time_stats(df):
     print('-'*40)
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -168,7 +152,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -186,7 +170,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
